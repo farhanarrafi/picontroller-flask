9208fe56cf8c1be5e3a3e06ed4747b05b526bf8e
Farhan Ar Rafi <farhanarrafi@gmail.com>
2018-02-04 21:54:37 +0600

    PI_CTRL_FLASK: Added UI components
    - Added manifest, icons and fa icons
    
    Refs - N/A

--- picontroller-flask.py
+++ picontroller-flask.py
@@ -1,11 +1,11 @@
-from flask import Flask
-
+from flask import Flask, render_template
+#from gpiozero import Button
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html', name = "Farhan")
 
 
 if __name__ == '__main__':
